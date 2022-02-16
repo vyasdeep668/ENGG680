f56953c2578961655ea2dee0a4974e34dc395fb4
Deep Vyas <vyasdeep668@gmail.com>
2022-02-16 13:55:08 -0700

    file change commit

--- Assignments/Assignment_3/A3_2_wikiQuotes.py
+++ Assignments/Assignment_3/A3_2_wikiQuotes.py
@@ -18,6 +18,7 @@ quotes_jason_dict = json.load(quotes_jason_file)
 def search_quote(name, quote):
     """ 
         TODO:
+        file changed
         2- Write a function search_quotes that takes a string as argument,
            searches the keys in the quotes dictionary for items containing 
            the search string and prints the matching name (key) and quotes.
